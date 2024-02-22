d9957a1c1aaafa6ef2b631e9dcb658d76d0adae4
Joshua Hickey <joshuah775@gmail.com>
2024-02-22 03:31:17 +0000

    Finish API

--- server.ts
+++ server.ts
@@ -3,17 +3,25 @@ import path from 'path';
 import { diary } from '.';
 import cron from 'node-cron';
 import cors from 'cors';
+import { getData } from './sheets';
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
+app.use(express.json());
 
+app.use(express.static('./client/build'))
 
 app.get('/api/diary/download', (req, res) => {
     const file = path.resolve(path.dirname(__dirname), 'diary.xlsx');
     res.download(file);
 });
+
+app.get('/api/diary', (req, res) => {
+    console.log('GET /api/diary');
+    res.json(getData())
+})
 
 app.post('/api/diary', (req, res) => {
     diary();
