dc7460d76fb65b4a1522b8bdd308838d5197bfa4
Joshua Hickey <joshuah775@gmail.com>
2024-02-27 01:09:04 +0000

    Adding header buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
     import MUITable from "../lib/Components/MUITable.svelte";
+
+    const download = () => {
+        console.log("Download");
+        window.location.href = "api/diary/download";
+    }
+
 </script>
 
 
@@ -7,7 +14,24 @@
 <div class="content">
     <header>
         <h1>Placement Diary</h1>
+        <div>
+            <button on:click={download}>
+                <Icon icon="material-symbols:download" style={'width: 3rem; height: 3rem'}/>
+            </button>
+            <button>
+                <Icon icon="material-symbols:upload" style={'width: 3rem; height: 3rem'}/>
+            </button>
+        </div>
+
     </header>
     <MUITable />
 </div>
 
+<style>
+    header {
+        margin: 0.3rem;
+        display: flex;
+        justify-content: space-between;
+    }
+
+</style>
