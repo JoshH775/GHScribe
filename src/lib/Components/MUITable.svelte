cf890205954f404284f3fa0a7cb8f3405835d542
Joshua Hickey <joshuah775@gmail.com>
2024-02-27 01:08:57 +0000

    Creating custom table

--- src/lib/Components/MUITable.svelte
+++ src/lib/Components/MUITable.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import type { DiaryEntry } from "$lib/types";
     import { onMount } from "svelte";
-    import DataTable, { Head, Body, Row, Cell} from '@smui/data-table'
+    import moment from "moment";
 
     let rows: DiaryEntry[] = [];
 
@@ -24,23 +24,53 @@
     });
 </script>
 
-<DataTable table$aria-label="User list" style="width: 100%;">
-    <Head>
-        <Row>
-            <Cell>Date</Cell>
-            <Cell>Work Carried Out</Cell>
-            <Cell>Knowledge Gained</Cell>
-            <Cell>Competencies</Cell>
-        </Row>
-    </Head>
-    <Body>
+<table>
+    <thead>
+        <tr>
+            <th class="date">Date</th>
+            <th class="wco">Work Carried Out</th>
+            <th class="kg">Knowledge Gained</th>
+            <th class="competencies">Competencies</th>
+        </tr>
+    </thead>
+    <tbody>
         {#each rows as row}
-            <Row>
-                <Cell>{row.Date}</Cell>
-                <Cell>{row["Work Carried Out"]}</Cell>
-                <Cell>{row["Knowledge Gained"]}</Cell>
-                <Cell>{row.Competencies}</Cell>
-            </Row>
+            <tr>
+                <td class="date">{moment(row.Date).format('LL')}</td>
+                <td class="wco">{row["Work Carried Out"]}</td>
+                <td class="kg">{row["Knowledge Gained"]}</td>
+                <td class="competencies">{row.Competencies}</td>
+            </tr>
         {/each}
-    </Body>
-</DataTable>
+</table>
+
+<style>
+    .date {
+        width: 10%;
+        text-align: center;
+    }
+
+    .wco {
+        width: 35%
+    }
+    
+    .kg {
+        width: 35%
+    }
+
+    .competencies {
+        width: 15%
+    }
+
+    td {
+        padding: 0.4rem;
+    }
+
+    th, td {
+        border: 1px solid black;
+    }
+
+    table {
+        border-collapse: collapse;
+    }
+</style>
